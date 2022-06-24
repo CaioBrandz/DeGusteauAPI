3e1d63a429bde5b595e0b5b26934dacda2b13dc1
CaioBrandz <caiobrandz@gmail.com>
2022-06-24 04:19:46 -0300

    Initializing server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/preferencias", require("./routes/preferencias"));
+app.use("/receitas", require("./routes/receitas"));
+app.use("/ingredientes", require("./routes/ingredientes"));
+
+/*
+app.use("/auth", require("./routes/jwtAuth"));
+*/
+
+app.listen(process.env.PORT || 5000);
+console.log("server on!");
